41974b38ccd6edd6b27aa129e7cfe7b2478c2c7e
uno <st9007a@gmail.com>
2017-06-25 11:06:54 +0800

    set collision sensor

---
+++ Assets/Scripts/CollisionCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCounter : MonoBehaviour {
+
+	public int counter;
+
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+	}
+
+	void OnTriggerEnter (Collider other) {
+		counter++;
+	}
+
+	void OnTriggerExit (Collider other) {
+		counter--;
+	}
+	
+}
--- Assets/Scripts/ThreePersionController.cs
+++ Assets/Scripts/ThreePersionController.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class ThreePersionController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject player;
+    public GameObject mainCamera;
+    public GameObject cameraCollisionBox;
+    public CollisionCounter cameraProbe;
+    public CollisionCounter frontProbe;
+
+    public float vertiaclRatio;
+    public float maxDistance;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update () {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 forward = mainCamera.transform.forward;
+        forward.y = 0;
+        forward.Normalize();
+
+        Vector3 distance = mainCamera.transform.position - player.transform.position;
+        distance.y = 0;
+
+        if (vertical > 0) {
+            player.GetComponent<Rigidbody>().velocity = forward * moveSpeed;
+            //Camera chases the player
+            if (distance.magnitude > maxDistance) {
+                float originY = cameraCollisionBox.transform.position.y;
+                Vector3 newPosition = player.transform.position + distance.normalized * maxDistance;
+                newPosition.y = originY;
+                cameraCollisionBox.transform.position = newPosition;
+            }
+
+        } else if (vertical < 0) {
+            player.GetComponent<Rigidbody>().velocity = -forward * moveSpeed;
+
+            //Camera chases the player
+            float originY = cameraCollisionBox.transform.position.y;
+            Vector3 newPosition = player.transform.position + distance.normalized * maxDistance;
+            newPosition.y = originY;
+            cameraCollisionBox.transform.position = newPosition;
+        } else {
+            player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+
+    void CalculateHeight () {
+        Vector3 originVector = mainCamera.transform.position;
+        Vector3 localVector = mainCamera.transform.localPosition;
+
+        originVector.y = player.transform.position.y;
+        localVector.y = maxDistance - (originVector - player.transform.position).magnitude;
+
+        mainCamera.transform.localPosition = localVector * vertiaclRatio;
+    }
 }
