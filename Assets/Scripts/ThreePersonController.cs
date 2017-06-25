70dff98f9417a609fc5ab2181d99f8e90f66061d
uno <st9007a@gmail.com>
2017-06-25 20:21:34 +0800

    correct script name and refactor file path

--- Assets/Scripts/ThreePersonController.cs
+++ Assets/Scripts/ThreePersonController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThreePersionController : MonoBehaviour {
+public class ThreePersonController : MonoBehaviour {
 
     public GameObject player;
     public GameObject mainCamera;
