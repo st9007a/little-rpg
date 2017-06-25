09e733d6eedf425b35597e5c7420e0baa3c8f7a5
uno <st9007a@gmail.com>
2017-06-25 10:19:32 +0800

    practice

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform lookAtObj;
+
+	void Update() {
+
+		transform.LookAt(lookAtObj);
+	}
+
+}
