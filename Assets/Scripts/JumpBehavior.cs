0c9991bffbfc56f85fae8e8effdadcc5aa0a6145
uno <st9007a@gmail.com>
2017-06-25 19:55:20 +0800

    fix bug when jump to wall , it won't fall immediatrly

--- Assets/Scripts/JumpBehavior.cs
+++ Assets/Scripts/JumpBehavior.cs
@@ -11,29 +11,30 @@ public class JumpBehavior : StateMachineBehaviour {
 	public bool isJump;
 	public float jump;
 
+	Rigidbody rb;
+
 	private float timer;
-	private float originX;
-	private float originZ;
 
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		timer = 0;
 		isJump = false;
-	    animator.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, animator.gameObject.GetComponent<Rigidbody>().velocity.y, 0);
+		rb = animator.gameObject.GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(0, animator.gameObject.GetComponent<Rigidbody>().velocity.y, 0);
 	}
 
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		if (timer < jumpPoint || timer > fallPoint) {
-			animator.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			rb.velocity = new Vector3(0, rb.velocity.y, 0);
 		}
 		if (timer >= jumpPoint && !isJump) {
 			isJump = true;
-			animator.gameObject.GetComponent<Rigidbody>().velocity += Vector3.up * jump;
+			rb.velocity += Vector3.up * jump;
 		}
 
 		if (animator.gameObject.GetComponent<Rigidbody>().velocity.y > 0) {
 			Vector3 newY = animator.gameObject.GetComponent<Rigidbody>().velocity;
 			newY.y -= gravity * Time.deltaTime;
-			animator.gameObject.GetComponent<Rigidbody>().velocity = newY;
+			rb.velocity = newY;
 		}
 		timer += Time.deltaTime;
 	}
