b191567d1087f397d39539c8b155f241f0c530af
uno <st9007a@gmail.com>
2017-06-28 19:41:24 +0800

    adjust ui

--- Assets/Views/ViewModels/DialogBox.cs
+++ Assets/Views/ViewModels/DialogBox.cs
@@ -2,6 +2,9 @@
 using MarkLight.Views.UI;
 
 public class DialogBox : UIView {
+	
+	public string message = "";
+	public bool active = false;
 	public void OnClick() {
 		Debug.Log("test");
 	}
--- Assets/Views/ViewModels/MainScene.cs
+++ Assets/Views/ViewModels/MainScene.cs
@@ -2,4 +2,5 @@
 using MarkLight;
 
 public class MainScene : View {
+
 }
