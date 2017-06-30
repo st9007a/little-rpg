61ad707fd63d7cb9ad6d4d5a4c1fd92b1ad9d427
uno <st9007a@gmail.com>
2017-06-30 18:41:10 +0800

    try to fix little movement when move

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,23 +6,8 @@ public class CameraScript : MonoBehaviour {
 
 	public Transform lookAtObj;
 
-	private float lastFrameX;
-
-	void Start() {
-		lastFrameX = transform.rotation.eulerAngles.x;
-	}
-
 	void Update() {
-
-		/*if (Mathf.Abs(lastFrameX - transform.rotation.eulerAngles.x) > 0.0001f) {
-			transform.LookAt(lookAtObj);
-		}*/
-
 		transform.LookAt(lookAtObj);
-		//Debug.Log(lastFrameX - transform.rotation.eulerAngles.x);
-		lastFrameX = transform.rotation.eulerAngles.x;
-
-
 	}
 
 }
